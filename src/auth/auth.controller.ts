6e6761b86bd8b6c3a0c92fa5cef868f52f5da3ce
tunatuna123 <tuna200538@gmail.com>
2024-03-03 16:32:49 +0900

    feat: Add GoogleAuthGuard to handleLogin and handleRedirect

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,13 +1,16 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, UseGuards } from '@nestjs/common';
+import { GoogleAuthGuard } from './utils/Guards';
 
 @Controller('auth')
 export class AuthController {
   @Get('google/login')
+  @UseGuards(GoogleAuthGuard)
   handleLogin() {
     return { msg: 'google login' };
   }
 
   @Get('google/redirect')
+  @UseGuards(GoogleAuthGuard)
   handleRedirect() {
     return { msg: 'OK' };
   }
