03ec85ae8525db99a1bb9a01220e7ffb40fa9790
tunatuna123 <tuna200538@gmail.com>
2024-03-03 16:25:17 +0900

    feat: Add GoogleStrategy to AuthModule

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
+import { GoogleStrategy } from './utils/GoogleStrategy';
 
 @Module({
-  providers: [AuthService],
+  providers: [AuthService, GoogleStrategy],
   controllers: [AuthController]
 })
 export class AuthModule {}
---
+++ src/auth/utils/GoogleStrategy.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@nestjs/common';
+import { PassportStrategy } from '@nestjs/passport';
+import { Strategy, Profile } from 'passport-google-oauth20';
+
+@Injectable()
+export class GoogleStrategy extends PassportStrategy(Strategy, 'google') {
+  constructor() {
+    super({
+      clientID: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      callbackURL: 'http://localhost:3000/auth/google/redirect',
+      scope: ['email', 'profile'],
+    });
+  }
+
+  validate(accessToken: string, refreshToken: string, profile: Profile) {
+    console.log('accessToken', accessToken);
+    console.log('refreshToken', refreshToken);
+    console.log('profile', profile);
+    return { profile, accessToken };
+  }
+}
